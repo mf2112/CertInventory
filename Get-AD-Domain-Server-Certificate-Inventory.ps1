3d038a2a0829a507d3f404a3c183437b7cf3f777
mf2112 <cpmf2112@gmail.com>
2018-03-24 19:40:22 -0700

    Small tweak
    Uncommented the title bar line

--- Get-AD-Domain-Server-Certificate-Inventory.ps1
+++ Get-AD-Domain-Server-Certificate-Inventory.ps1
@@ -1,7 +1,7 @@
 ﻿$staledate = (Get-Date).AddDays(-90)
 $computers = Get-ADComputer -Filter {(OperatingSystem -Like "*Server*") -and (Enabled -eq $True) -and (LastLogonDate -ge $staledate) -and (Modified -ge $staledate) -and (PasswordLastSet -ge $staledate) -and (whenChanged -ge $staledate) -and (serviceprincipalname -ne "*MSClusterVirtualServer*")} | Select name -expandproperty name | Where {(Resolve-DNSName $_.name -ea 0) -and (Test-Connection -ComputerName $_.Name -Count 1 -ea 0)} | Sort
 $inv = @()
-#$inv += "Computer" + ";" + "Subject" + ";" + "SAN" + ";" + "Thumbprint" + ";" + "Issuer Name" + ";" + "Valid Until" + ";" + "Days to Expiration"
+$inv += "Computer" + ";" + "Subject" + ";" + "SAN" + ";" + "Thumbprint" + ";" + "Issuer Name" + ";" + "Valid Until" + ";" + "Days to Expiration"
 foreach ($computer in $computers){
 Invoke-Command -ComputerName $computer -ScriptBlock {
 Set-Location Cert:\LocalMachine\My
